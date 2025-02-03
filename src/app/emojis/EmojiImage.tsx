7349e0e3aa688080075375e9e7b24822eb218071
catpleademoji <catpleademoji@gmail.com>
2025-02-03 20:13:29 +0000

    Remove cat cowboy

--- src/app/emojis/EmojiImage.tsx
+++ src/app/emojis/EmojiImage.tsx
@@ -4,7 +4,7 @@ import { EmojiModel } from "./Emojis";
 type EmojiProps = EmojiModel;
 export function EmojiImage({ name, alt, display }: EmojiProps) {
     return (
-        <Image
+        <img
             src={`./images/${name}.svg`}
             alt={alt || ""}
             title={display} />
