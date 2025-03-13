2d9f5cd65084bcc302ff341e8494419a3cbb8d00
catpleademoji <catpleademoji@gmail.com>
2025-03-13 00:42:00 +0000

    update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,11 +5,12 @@ export default function AboutPage() {
                 <h1>About me</h1>
                 <p>hiii!! I'm catpleademoji. I'm a cat-puppy guy :3</p>
                 <p>I make little edits of the cat Twemojis.</p>
-                <p>I like games and hopefully I can share something soon.</p>
+                <p>I also make games! You can check out some of them <a href="/games">here</a>.</p>
             </section>
             <section>
                 <h1>Can you use the cat emojis?</h1>
-                <p>Yeah! You can use them for pretty much anything you want. Just don't claim them as your own.</p>
+                <p>Yeah! You can use them for pretty much anything you want. Just don't claim them as your own please.</p>
+                <p>You can download them from my <a href="https://github.com/catpleademoji/catpleademoji.neocities/releases/latest">github</a>.</p>
             </section>
         </>
     )
